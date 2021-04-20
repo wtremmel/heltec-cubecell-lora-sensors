ef2b6434d9062bce129cbc3fe72452889f09f7ef
Wolfgang Tremmel <github@garf.de>
2021-04-20 20:54:09 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,417 @@
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+#include <Wire.h>
+#include <ArduinoLog.h>
+#include "CubeCell_NeoPixel.h"
+
+
+
+#include "cubecell.h"
+
+#include <CayenneLPP.h>
+CayenneLPP lpp(51);
+
+// Sensor Libraries
+#include "Adafruit_Si7021.h"
+#include "Adafruit_BME280.h"
+#include "Adafruit_TSL2561_U.h"
+#include <Max44009.h>
+
+
+// Global Objects
+Adafruit_Si7021 si7021;
+Max44009 gy49(0x4a);
+Adafruit_BME280 bme280;
+Adafruit_TSL2561_Unified tsl2561 = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT);
+CubeCell_NeoPixel pixels(1, RGB, NEO_GRB + NEO_KHZ800);
+
+bool bme280_found = false;
+bool voltage_found = true;
+bool gy49_found = false;
+
+bool setup_complete = false;
+
+uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
+uint8_t nwkSKey[] = { 0x15, 0xb1, 0xd0, 0xef, 0xa4, 0x63, 0xdf, 0xbe, 0x3d, 0x11, 0x18, 0x1e, 0x1e, 0xc7, 0xda,0x85 };
+uint8_t appSKey[] = { 0xd7, 0x2c, 0x78, 0x75, 0x8c, 0xdc, 0xca, 0xbf, 0x55, 0xee, 0x4a, 0x77, 0x8d, 0x16, 0xef,0x67 };
+uint32_t devAddr;
+/*LoraWan region, select in arduino IDE tools*/
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
+
+/*LoraWan Class, Class A and Class C are supported*/
+DeviceClass_t  loraWanClass = LORAWAN_CLASS;
+
+/*the application data transmission duty cycle.  value in [ms].*/
+uint32_t appTxDutyCycle = 2*60*1000;
+
+/*OTAA or ABP*/
+bool overTheAirActivation = LORAWAN_NETMODE;
+
+/*ADR enable*/
+bool loraWanAdr = LORAWAN_ADR;
+
+/* set LORAWAN_Net_Reserve ON, the node could save the network info to flash, when node reset not need to join again */
+bool keepNet = LORAWAN_NET_RESERVE;
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = LORAWAN_UPLINKMODE;
+
+/* Application port */
+uint8_t appPort = 2;
+
+uint8_t ledr = 0, ledg = 0, ledb = 0;
+bool ledon = false;
+
+/*!
+* Number of trials to transmit the frame, if the LoRaMAC layer did not
+* receive an acknowledgment. The MAC performs a datarate adaptation,
+* according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+* to the following table:
+*
+* Transmission nb | Data Rate
+* ----------------|-----------
+* 1 (first)       | DR
+* 2               | DR
+* 3               | max(DR-1,0)
+* 4               | max(DR-1,0)
+* 5               | max(DR-2,0)
+* 6               | max(DR-2,0)
+* 7               | max(DR-3,0)
+* 8               | max(DR-3,0)
+*
+* Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+* the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+*/
+uint8_t confirmedNbTrials = 4;
+
+//
+// Scan for sensors
+//
+void setup_i2c() {
+  byte error, address;
+  unsigned int devices=0;
+
+// 0x29 TSL45315 (Light)
+// 0x38 VEML6070 (Light)
+// 0x39 TSL2561
+// 0x40 SI7021
+// 0x48 4*AD converter
+// 0x4a GY49 or MAX44009 Light Sensor
+// 0x50 PCF8583P
+// 0x57 ATMEL732
+// 0x68 DS3231 Clock
+// 0x76 BME280
+// 0x77 BME680 (also BMP180)
+
+  Log.verbose("Scanning i2c bus");
+  Wire.begin();
+  // Wire.setClock(10000);
+  for(address = 1; address < 127; address++ ) {
+    Log.verbose(F("Trying 0x%x"),address);
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Log.verbose(F("I2C device found at address 0x%x !"),address);
+      devices++;
+    }
+    if (address == 0x4a) {
+      // GY-49
+      float l = gy49.getLux();
+      if (! gy49.getError()) {
+        gy49_found = true;
+        Log.notice(F("GY49 found at 0x%x, current Lux %F"),address,l);
+      }
+    }
+
+    if (address == 0x76) {
+      // BME280
+      bme280_found = bme280.begin(address);
+      Log.verbose(F("BME280 found? %T"),bme280_found);
+    }
+
+  }
+  Log.verbose(F("i2c bus scanning complete, %d devices"),devices);
+}
+
+void set_led(uint8_t r, uint8_t g, uint8_t b) {
+  pixels.setPixelColor(0, pixels.Color(r,g,b));
+  pixels.show();
+}
+
+// BME280 routines
+void read_bme280() {
+  Log.verbose(F("read_bme280"));
+  lpp.addTemperature(1,bme280.readTemperature());
+  lpp.addRelativeHumidity(2,bme280.readHumidity());
+  lpp.addBarometricPressure(3,bme280.readPressure() / 100.0F);
+}
+
+void read_gy49() {
+  Log.verbose(F("read_gy49"));
+  lpp.addLuminosity(4,gy49.getLux());
+}
+
+// Battery voltage
+void read_voltage() {
+  uint16_t v = getBatteryVoltage();
+  lpp.addAnalogInput(5,(float)v / 100.0);
+  Log.verbose(F("Voltage: %d"),v);
+}
+
+// Sensor routines
+void read_sensors() {
+  lpp.reset();
+
+  // switch on power
+  digitalWrite(Vext,LOW);//set vext to high
+  if (ledon) {
+    set_led(ledr,ledg,ledb);
+    delay(1000);
+  }
+
+  delay(100);
+  // initialize sensors
+  setup_i2c();
+
+  if (bme280_found) {
+    read_bme280();
+  }
+  if (gy49_found) {
+    read_gy49();
+  }
+
+  if (voltage_found) {
+    read_voltage();
+  }
+#if 0
+  if (si7021_found) {
+    read_si7021();
+  }
+  if (tsl2561_found) {
+    read_tsl2561();
+  }
+  if (ads1115_found) {
+    read_ads1115();
+  }
+#endif
+Wire.end();
+// switch off power
+digitalWrite(Vext,HIGH);//set vext to low
+
+}
+
+void setup_serial() {
+  Serial.begin(115200);
+#if DEBUG
+  while (!Serial);
+#endif
+}
+
+
+// Logging helper routines
+void printTimestamp(Print* _logOutput) {
+  char c[12];
+  sprintf(c, "%10lu ", TimerGetCurrentTime());
+  _logOutput->print(c);
+}
+
+void printNewline(Print* _logOutput) {
+  _logOutput->print('\n');
+}
+
+void setup_logging() {
+  Log.begin(LOG_LEVEL_VERBOSE, &Serial);
+  Log.setPrefix(printTimestamp);
+  Log.setSuffix(printNewline);
+  Log.verbose("Logging has started");
+}
+
+void setup_lora() {
+  Log.verbose(F("setup_lora: start"));
+  deviceState = DEVICE_STATE_INIT;
+	LoRaWAN.ifskipjoin();
+}
+
+void setup() {
+  setup_serial();
+  delay(5000);
+  setup_logging();
+
+  // Turn on power for devices
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext,LOW);//set vext to high
+
+  setup_i2c();
+  setup_lora();
+
+  pixels.begin();
+  pixels.clear();
+}
+
+static void prepareTxFrame( ) {
+  read_sensors();
+
+  appDataSize = lpp.getSize();
+  memcpy(appData,lpp.getBuffer(),appDataSize);
+}
+
+// -------------- Command Processing -----------------
+void process_system_led_command(unsigned char len, unsigned char *buffer) {
+  if (len == 0) {
+    Log.error(F("Zero length LED command"));
+    return;
+  } else {
+    Log.verbose(F("Processing LED command"));
+  }
+
+  switch (buffer[0]) {
+    case 0x00:
+      pixels.clear();
+      ledon = false;
+      pixels.show();
+      break;
+    case 0x02:
+      if (len == 4) {
+        // do rgb magic
+        ledr = buffer[1];
+        ledg = buffer[2];
+        ledb = buffer[3];
+        if (ledr == 0 && ledg == 0 && ledb == 0) {
+          ledon = false;
+        } else {
+          ledon = true;
+        }
+        set_led(ledr,ledg,ledb);
+      } else {
+        Log.error(F("Missing RGB values for LED. Len = %d"),len);
+      }
+      break;
+    default:
+      Log.error(F("Unknown LED command %d"), buffer[0]);
+      break;
+  }
+}
+
+
+void process_system_command(unsigned char len, unsigned char *buffer) {
+  if (len == 0) {
+    Log.error(F("Zero length system command"));
+    return;
+  } else {
+    Log.verbose(F("Processing system command"));
+  }
+  switch (buffer[0]) {
+    case 0x03:
+      process_system_led_command(len-1,buffer+1);
+      break;
+    case 0xff:
+      // Reboots
+      Log.notice(F("Executing reboot command"));
+      delay(100);
+      HW_Reset(0);
+    default:
+      Log.error(F("Unknown system command %d"),buffer[0]);
+      break;
+  }
+}
+
+void process_sensor_bme280(unsigned char len, unsigned char *buffer) {
+  if (len == 0) {
+    Log.error(F("Zero length bme280 command"));
+    return;
+  }
+
+}
+
+void process_sensor_command(unsigned char len, unsigned char *buffer) {
+  if (len == 0) {
+    Log.error(F("Zero length sensor command"));
+    return;
+  }
+  switch (buffer[0]) {
+    case 0x11:
+      process_sensor_bme280(len-1,buffer+1);
+      break;
+    default:
+      Log.error(F("Unknown sensor command %d"),buffer[0]);
+      break;
+  }
+}
+
+void process_received_lora(unsigned char len, unsigned char *buffer) {
+  if (len == 0)
+    return;
+
+  Log.verbose(F("Processing %d bytes of received data"),len);
+  switch (buffer[0]) {
+    case 0:
+      process_system_command(len-1,buffer+1);
+      break;
+    case 1:
+      process_sensor_command(len-1,buffer+1);
+      break;
+    default:
+      Log.error(F("Unknown command %d"),buffer[0]);
+      break;
+  }
+}
+
+
+void downLinkDataHandle(McpsIndication_t *mcpsIndication)
+{
+  Log.verbose(F("+REV DATA:%s,RXSIZE %d,PORT %d\r\n"),
+    mcpsIndication->RxSlot?"RXWIN2":"RXWIN1",
+    mcpsIndication->BufferSize,
+    mcpsIndication->Port);
+  process_received_lora(mcpsIndication->BufferSize, mcpsIndication->Buffer);
+}
+
+
+
+void loop() {
+  setup_complete = true;
+  switch( deviceState )
+	{
+		case DEVICE_STATE_INIT:
+		{
+			LoRaWAN.generateDeveuiByChipID();
+			printDevParam();
+			LoRaWAN.init(loraWanClass,loraWanRegion);
+			deviceState = DEVICE_STATE_JOIN;
+			break;
+		}
+		case DEVICE_STATE_JOIN:
+		{
+			LoRaWAN.join();
+			break;
+		}
+		case DEVICE_STATE_SEND:
+		{
+			// prepareTxFrame( appPort );
+      prepareTxFrame();
+			LoRaWAN.send();
+			deviceState = DEVICE_STATE_CYCLE;
+			break;
+		}
+		case DEVICE_STATE_CYCLE:
+		{
+			// Schedule next packet transmission
+			txDutyCycleTime = appTxDutyCycle + randr( 0, APP_TX_DUTYCYCLE_RND );
+			LoRaWAN.cycle(txDutyCycleTime);
+			deviceState = DEVICE_STATE_SLEEP;
+			break;
+		}
+		case DEVICE_STATE_SLEEP:
+		{
+			LoRaWAN.sleep();
+			break;
+		}
+		default:
+		{
+			deviceState = DEVICE_STATE_INIT;
+			break;
+		}
+	}
+
+}
